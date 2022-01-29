c034b213df685f71a1b6e550026b8138f732021d
Breno Papalardi <brenop.developer@gmail.com>
2022-01-29 17:48:02 -0300

    estrutura inicial + material.ui

---
+++ src/theme.js
@@ -0,0 +1,11 @@
+import { createMuiTheme} from '@material-ui/core/styles'
+
+const theme = createMuiTheme({
+    pallete: {
+        primary: {
+            main: '#000000',
+        }
+    }
+})
+
+export default theme
